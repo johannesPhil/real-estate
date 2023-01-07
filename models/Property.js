ac5e4e109ed97d666a2cb06dcd86622a2d3ede8f
Johannsephil <jay3pee@gmail.com>
2023-01-07 19:46:44 +0100

    add and edit fields for property model

--- models/Property.js
+++ models/Property.js
@@ -1,6 +1,18 @@
-import { Schema, model } from "mongoose";
+import mongoose from "mongoose";
 
-const PropertySchema = new Schema({
+const residence = ["self-contained", "flat", "house"];
+const space = ["land", "commercial", "event venue"];
+
+function isRequired(type) {
+	if (type == "residence") {
+		return residence.includes(this.type);
+	}
+	if (type == "space") {
+		return space.includes(this.type);
+	}
+}
+
+const PropertySchema = new mongoose.Schema({
 	title: {
 		type: String,
 		required: [true, "Property title required"],
@@ -11,26 +23,40 @@ const PropertySchema = new Schema({
 	},
 	type: {
 		type: String,
-		enum: [
-			"self-contained",
-			"flat",
-			"house",
-			"land",
-			"commercial",
-			"event venue",
-		],
+		enum: [...residence, ...space],
 		required: [true, "Property type required"],
 	},
 	location: {
 		type: String,
-		required: [true, "Property adress required"],
+		required: [true, "Property address required"],
 	},
-	rooms: Number,
-	bathrooms: Number,
-	toilets: Numbers,
+	thumbnail: {
+		type: String,
+		required: [true, "Thumbnail for property required"],
+	},
+	pictures: {
+		type: [String],
+		required: [true, "Upload pictures for the property"],
+	},
+	room: {
+		type: { Number },
+		required: [isRequired("residence"), "Number of rooms required"],
+	},
+	bathroom: {
+		type: { Number },
+		required: [isRequired("residence"), "Number of bathrooms required"],
+	},
+	toilet: {
+		type: { Number },
+		required: [isRequired("residence"), "Number of toilets required"],
+	},
 	parking: Number,
+	size: {
+		type: { String },
+		required: [isRequired("space"), "Space size required"],
+	},
 });
 
-const Property = model("Property", PropertySchema);
+const Property = mongoose.model("Property", PropertySchema);
 
-export default Property;
+module.exports = mongoose.models.Property || Property;
