4a850a17cbe64c02e8fab19b6d561cf6510bfc4e
Johannsephil <jay3pee@gmail.com>
2023-01-07 19:48:45 +0100

    start handling property creation

---
+++ controllers/PropertyController.js
@@ -0,0 +1,16 @@
+import { errorHandler } from "../helpers/errorHandler";
+import Property from "../models/Property";
+
+export const addProperty = async (req, res) => {
+	try {
+		let newProperty = new Property(req.body);
+		let dbProperty = await newProperty.save();
+
+		if (!dbProperty) {
+			throw "Could not save property. Please try again";
+		}
+		return res.status(200).json({ status: "success", data: dbProperty });
+	} catch (error) {
+		errorHandler(error, res);
+	}
+};
