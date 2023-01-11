0b027f1f360a282f4eacc2c78025c339e9256df5
Johannsephil <jay3pee@gmail.com>
2023-01-11 01:18:21 +0100

    validate property

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -4,6 +4,7 @@ import isEmail from "validator/lib/isEmail";
 import { errorHandler } from "../helpers/errorHandler";
 import isLength from "validator/lib/isLength";
 import User from "../models/User";
+import { localUpload } from "../helpers/fileUpload";
 
 export async function validateUserData(req, res, next) {
 	try {
@@ -47,8 +48,47 @@ export async function validateCredentials(req, res, next) {
 }
 
 export async function validateProperty(req, res, next) {
-	const { title, description, type, location, thumbnail, pictures } =
-		req.body;
+	try {
+		const {
+			title,
+			description,
+			type,
+			location,
+			room,
+			bathroom,
+			toilet,
+			size,
+			parking,
+		} = req.body;
+		const images = req.files;
+		const imageFormats = ["image/png", "image/jpeg", "image/jpg"];
+		const residence = ["self-contained", "flat", "house"];
+		const space = ["land", "commercial", "event venue"];
 
-	return next();
+		if (residence.includes(type) && (!room || !bathroom || !toilet)) {
+			throw `${type} property must have room, bath and toilet specified`;
+		}
+
+		if (space.includes(type) && !size) {
+			throw `${type} property must have size specified`;
+		}
+		if (!title || !description || !type || !location) {
+			throw "Fill all property description fields";
+		}
+		if (images.some((image) => imageFormats.indexOf(image.mimetype) < 0)) {
+			throw "Unsupported file format detected in the upload";
+		}
+
+		if (!images.some((image) => image.fieldname === "thumbnail")) {
+			throw "Add THUMBNAIL for property";
+		}
+
+		if (!images.some((image) => image.fieldname === "pictures")) {
+			throw "Upload at least one PICTURE for the property";
+		}
+
+		return next();
+	} catch (error) {
+		errorHandler(error, res);
+	}
 }
