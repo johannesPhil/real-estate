f4e8d5a222c411d45e564d326a821f8b0615a1fa
Johannsephil <jay3pee@gmail.com>
2023-01-11 01:21:28 +0100

    addb  route for adding property

--- pages/api/properties/index.js
+++ pages/api/properties/index.js
@@ -1,9 +1,26 @@
 import nextConnect from "next-connect";
 import { addProperty } from "../../../controllers/PropertyController";
 import { validateProperty } from "../../../middlewares/validation";
+import multer from "multer";
+import localUpload, {
+	memUpload,
+	picturesUpload,
+	thumbnailUpload,
+} from "../../../helpers/fileUpload";
+
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+};
 
 const propertyRouteHandler = nextConnect();
-propertyRouteHandler.use(validateProperty);
+propertyRouteHandler.use(
+	multer().any(),
+	// thumbnailUpload.single("thumbnail"),
+	// picturesUpload.array("pictures"),
+	validateProperty
+);
 
 propertyRouteHandler.post(addProperty);
 
