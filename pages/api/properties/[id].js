350a15888148f11abb5e9273c4555360e2665665
Johannsephil <jay3pee@gmail.com>
2023-01-17 01:42:36 +0100

    add property update controller

--- pages/api/properties/[id].js
+++ pages/api/properties/[id].js
@@ -3,21 +3,22 @@ import nextConnect from "next-connect";
 import { verifyToken } from "../../../middlewares/jwt";
 import { errorHandler } from "../../../helpers/errorHandler";
 import dbConnection from "../../../helpers/dbConnection";
+import {
+	editProperty,
+	fetchProperty,
+} from "../../../controllers/PropertyController";
+import multer from "multer";
 
 const propertyIdHandler = nextConnect();
 
-propertyIdHandler.get(verifyToken, async (req, res) => {
-	await dbConnection();
-	const {
-		query: { id },
-	} = req;
-	Property.findById(id, (err, property) => {
-		if (property) {
-			return res.json(property);
-		} else {
-			errorHandler(err, res);
-		}
-	});
-});
+propertyIdHandler
+	.use(verifyToken)
+	.get(fetchProperty)
+	.put(multer().any(), editProperty);
 
 export default propertyIdHandler;
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+};
